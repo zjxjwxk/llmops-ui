7bd5b900b7224d0f984265d554dd2b4b9b4667d6
zjxjwxk <zjxjwxk@gmail.com>
2026-03-21 15:46:02 +0800

    Import ArcoDesign and Tailwind CSS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ArcoVue from '@arco-design/web-vue'
+import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+
+import '@arco-design/web-vue/dist/arco.css'
+import '@/assets/styles/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ArcoVue)
+app.use(ArcoVueIcon)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold underline text-blue-700">This is Xinkang Wu</h1>
+    <a-button type="primary">注册登录</a-button>
+    <a-avatar :style="{ backgroundColor: '#3370ff' }">
+      <IconUser />
+    </a-avatar>
   </div>
 </template>
 
